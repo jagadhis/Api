a2f90fd4d5fc92cf83c6d5a15c4636317d19eff1
jagadhis <jwaran78@gmail.com>
2022-02-20 11:27:32 +0530

    birthday commit

--- Backend/Controllers/todocontroller.js
+++ Backend/Controllers/todocontroller.js
@@ -1,32 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc get todo
 //@route GET/api/greeting/
 //@access Private
-const gettodo = (req,res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Please add a todo')
-    }
-    res.status(200).json({message:"hey Codie"})
-}
+const gettodo = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a todo");
+  }
+  res.status(200).json({ message: "hey Codie" });
+});
 //@desc Set todo
 //@route GET/api/greeting/
 //@access Private
-const Settodo = (req,res) => {
-    res.status(200).json({message:"hello there"})
-}
+const Settodo = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "hello there" });
+});
 //@desc Update todo
 //@route GET/api/greeting/
 //@access Private
-const Updatetodo = (req,res) => {
-    res.status(200).json({message:`Update message ${req.params.id}`})
-}
+const Updatetodo = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update message ${req.params.id}` });
+});
 //@desc Delete todo
 //@route GET/api/greeting/
 //@access Private
-const Deletetodo = (req,res) => {
-    res.status(200).json({message:`Delete message ${req.params.id}`})
-}
+const Deletetodo = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete message ${req.params.id}` });
+});
 
-module.exports ={
-    gettodo,Settodo,Updatetodo,Deletetodo,
-}
+module.exports = {
+  gettodo,
+  Settodo,
+  Updatetodo,
+  Deletetodo,
+};
